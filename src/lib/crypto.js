8a0422dbaaf59b47ec6a48fb034b65bfd3d36493
Oleg Litvin <olitvin@zuzex.com>
2017-06-13 09:58:15 +0300

    add method for creating key

--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -2,6 +2,17 @@
 var Crypto = require('crypto');
 
 angular.module('olitvin.crypto', [])
-    .factory('zCrypto', function () {
-        return Crypto;
+    .factory('zCrypto', function () {        
+        return angular.extend(Crypto, {
+            newKey: function (keyHex)
+            {
+                key = Crypto.createECDH('prime256v1');
+                if (keyHex) {
+                    key.setPrivateKey(Buffer(keyHex, 'hex'));
+                } else {
+                    key.generateKeys('hex');
+                }
+                return key;
+            }
+        });
     })
